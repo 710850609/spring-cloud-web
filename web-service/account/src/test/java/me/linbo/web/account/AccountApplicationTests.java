b350c146f00c5117ef409c57bf07004ab6074338
lin_bo <710850609@qq.com>
2019-11-20 18:40:25 +0800

    新增account模块

---
+++ web-service/account/src/test/java/me/linbo/web/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package me.linbo.web.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
